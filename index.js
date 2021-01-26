3b49f33e1bc33d0179209af4b7afa94d80849b16
Sam Wheeler <rowan5643@gmail.com>
2021-01-26 18:16:54 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function shout(string){
+  return string.toUpperCase()
+}
+function whisper(string){
+  return string.toLowerCase()
+}
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+var lowercase = "hello!"
+var uppercase = "HELLO!"
+var mixedcase ='I love you, Grandma!'
+function sayHiToGrandma(string){
+  if (lowercase.toLowerCase()===lowercase) {return 'I can\'t hear you!'}
+  if (lowercase.toUpperCase()===uppercase) {return 'YES INDEED!'}
+}
